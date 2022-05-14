f6e8cb28bfc28935049589a225a2933736a5bd5a
amirzhanq <amirzhanq@gmail.com>
2022-05-14 15:35:09 +0600

    error levels

--- checker/error.go
+++ checker/error.go
@@ -3,8 +3,23 @@ package helper
 import "log"
 
 // ErrCheck Switch case log fatal,println, and others
-func ErrCheck(err error, message string) {
+func ErrCheck(err error, message string, level string) {
+
 	if err != nil {
-		log.Fatal(message, err)
+		switch level {
+		case "":
+			log.Println(message, err)
+		case "Info":
+			log.Println("Info", message, err)
+
+		case "Fatal":
+			log.Fatal("Fatal", message, err)
+		case "Panic":
+			log.Panic("Panic", message, err)
+			//case "Trace":
+			//case "Debug":
+			//case "Warn":
+			//case "Error":
+		}
 	}
 }
